17e98a71513050f15a32137275b817159ab51010
Aleksandr Rudenko <TheOne20001@gmail.com>
2011-02-26 11:56:29 -0800

    more chore work

--- Sasha/ChoreMongerTest/src/com/choremonger/test/Chore.java
+++ Sasha/ChoreMongerTest/src/com/choremonger/test/Chore.java
@@ -15,6 +15,10 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.io.UnsupportedEncodingException;
 
+
+
+
+
 public class Chore implements Chore
 {
 	List<User> users_assigned;
@@ -24,7 +28,8 @@ public class Chore implements Chore
 	double points;
 	String status;
 	double priority;
-
+	String str;
+394 uri=\”/user/394\” /><user id=403 uri=\”/user/403\” /></users>
 	public Chore()
 	{
 		this(null, null, null, 0.0, null, 0.0);
@@ -43,6 +48,15 @@ public class Chore implements Chore
 	}
 	public Chore(String name, String id, List<User> users, double points, String intsructions, double priority)
 	{
+		Iterator itr = users.iterator();
+		str = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><chore id=\"\"><instructions>"+this.intsructions+"</instructions><name>"+this.name+"</name><pointValue>"+Double.toString(this.points)+"</pointValue>
+		for (users.hasNext())
+		{ 
+			String temp = Double.toString(itr.next().getId());
+			str.concat("<users><user id=").concat(temp).concast(" uri=\”/user/").concat(temp).concat(\” />);
+		}
+		str.concat("</users><priority>"+Double.toString(this.priority)+"</priority><status>"+this.status+"</status></chore>");
+
 		this.id = id;
 		this.name = name;
 		this.users_assigned = users;
@@ -53,7 +67,7 @@ public class Chore implements Chore
 	}
 	public Chore(String id)
 	{
-		String str, value;
+		String value;
 		this.id = id;
 		HttpGet request = new HttpGet(HttpRequestExecutor.RESOURCE_ROOT + "chore/" + this.id);
 		HttpResponse response = HttpRequestExecutor.executeRequest(request);
@@ -62,13 +76,13 @@ public class Chore implements Chore
 		if (response.getStatusLine().getStatusCode() == 200)
 		{
 			try {
-				str = response.getEntity().getContent();
+				this.str = response.getEntity().getContent();
 				if ((str.indexOf("<name>") != -1)
-					this.name = str.substring(str.indexOf("<name>")+6, str.indexOf("</name>"));					//name
+					this.name = str.substring(str.indexOf("<name>")+6, str.indexOf("</name>"));						//name
 				else 
 					this.name = "";
 				if ((str.indexOf("<intsructions>") != -1)
-					this.intsructions = str.substring(str.indexOf("<intsructions>")+14, str.indexOf("</intsructions>"));		//instructions
+					this.intsructions = str.substring(str.indexOf("<intsructions>")+14, str.indexOf("</intsructions>"));			//instructions
 				else 
 					this.intsructions = "";
 				if ((str.indexOf("<status>") != -1)
@@ -76,11 +90,11 @@ public class Chore implements Chore
 				else 
 					this.status = "";
 				if ((str.indexOf("<points>") != -1)
-					this.points = Double.parseDouble(str.substring(str.indexOf("<points>")+8, str.indexOf("</points>")));		//points
+					this.points = Double.parseDouble(str.substring(str.indexOf("<points>")+8, str.indexOf("</points>")));			//points
 				else 
 					this.points = 0.0;
 				if ((str.indexOf("<priority>") != -1)
-					this.priority = Double.parseDouble(str.substring(str.indexOf("<priority>")+10, str.indexOf("</priority>")));	//priority
+					this.priority = Double.parseDouble(str.substring(str.indexOf("<priority>")+10, str.indexOf("</priority>")));		//priority
 				else 
 					this.priority = 0.0;
 				//users
@@ -120,6 +134,7 @@ public class Chore implements Chore
 	}
 	public void setStatus(String status)
 	{
+		this.str = str.replace(this.status, status);
 		this.status = status;
 		if (this.status  = "Completed")
 		{
@@ -127,6 +142,7 @@ public class Chore implements Chore
 			while(itr.hasNext())
 				//itr.give_reward(this.points);
 		}
+
 	}
 	public String getPriority()
 	{
@@ -134,10 +150,11 @@ public class Chore implements Chore
 	}
 	public void setPriority(double priority)
 	{
+		this.str = str.replace(this.priority, Double.toString(priority));
 		this.priority = priority;
 	}
 
-	public void addUser(User toAdd)
+/**/	public void addUser(User toAdd)
 	{
 		this.users_assigned.add(toAdd);		
 	}
@@ -162,25 +179,29 @@ public class Chore implements Chore
 		return this.users_assigned;
 	}
 
-	public boolean removeUser(User toRemove)
+/**/	public boolean removeUser(User toRemove)
 	{
 		return this.users_assigned.remove(toRemove);
 	}
 
 	public void setId(String newId)
 	{
+		this.str = str.replace(this.id, newId);
 		this.id = newId;
 	}
 	public void setInstructions(String newInstructions)
 	{
+		this.str = str.replace("<intsructions>"+this.intsructions, "<intsructions>"+newInstructions);
 		this.intsructions = newInstructions;
 	}
 	public void setName(String newName)
 	{
+		this.str = str.replace("<name>"+this.name, "<name>"+newName);
 		this.name = newName;
 	}
 	public void setPointValue(double newPointValue)
 	{
+		this.str = str.replace("<pointValue>"+this.points, "<pointValue>"+Double.toString(newPointValue));
 		this.points = newPointValue;
 	}
 
