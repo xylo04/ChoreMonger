2992908f1b24c6903dd1f2c3977204a503f16fb1
Aleksandr Rudenko <TheOne20001@gmail.com>
2011-04-19 07:09:54 -0700

    new stuff

--- Sasha/ChoreMongerTest/src/com/choremonger/test/FamilyTestDriver.java
+++ Sasha/ChoreMongerTest/src/com/choremonger/test/FamilyTestDriver.java
@@ -9,11 +9,20 @@ public class FamilyTestDriver {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Driver is asking to get Chore");
+		String temp;
 		Chore family = new ChoreImpl();
 
 		System.out.println("\n\nMade family:");
-		System.out.println("Name: "+family.getName());
+		System.out.println("Name: "+ family.getName());
 		System.out.println("Id:" + family.getId());
+		temp = family.getId();
+		family.setName("New Chore");
+		System.out.println("Name: "+ family.getName());
+		System.out.println("retrevng chore by id");
+		Chore chore = new ChoreImpl(temp);
+		System.out.println("Name: "+ chore.getName());
+		System.out.println("Id:" + chore.getId());
+		
 
 	}
 
